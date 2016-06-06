a377ea7408b0eb08d2342332dc5c9c8be812c3ea
Guillaume Lefranc <guillaume@dragonscale.eu>
2016-06-06 15:56:00 +0200

    Dashboard mode change + interval fix

--- httpserver.go
+++ httpserver.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type settings struct {
+	Interactive string `json:"interactive"`
+}
+
 func httpserver() {
 	http.HandleFunc("/", handlerApp)
 	http.HandleFunc("/servers", handlerServers)
+	http.HandleFunc("/master", handlerMaster)
 	http.HandleFunc("/log", handlerLog)
 	http.HandleFunc("/switchover", handlerSwitchover)
+	http.HandleFunc("/failover", handlerFailover)
+	http.HandleFunc("/interactive", handlerInteractiveToggle)
+	http.HandleFunc("/settings", handlerSettings)
 	log.Println("Starting agent on port " + httpport)
 	http.ListenAndServe(bindaddr+":"+httpport, nil)
 }
@@ -29,6 +38,28 @@ func handlerServers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerMaster(w http.ResponseWriter, r *http.Request) {
+	e := json.NewEncoder(w)
+	err := e.Encode(master)
+	if err != nil {
+		log.Println("Error encoding JSON: ", err)
+		http.Error(w, "Encoding error", 500)
+		return
+	}
+}
+
+func handlerSettings(w http.ResponseWriter, r *http.Request) {
+	s := new(settings)
+	s.Interactive = fmt.Sprintf("%v", interactive)
+	e := json.NewEncoder(w)
+	err := e.Encode(s)
+	if err != nil {
+		log.Println("Error encoding JSON: ", err)
+		http.Error(w, "Encoding error", 500)
+		return
+	}
+}
+
 func handlerLog(w http.ResponseWriter, r *http.Request) {
 	e := json.NewEncoder(w)
 	err := e.Encode(tlog.buffer)
@@ -40,11 +71,25 @@ func handlerLog(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerSwitchover(w http.ResponseWriter, r *http.Request) {
-	if master.State != stateFailed || failCount > 0 {
+	if master.State != stateFailed && failCount == 0 {
 		masterFailover(false)
 		return
 	}
 	logprint("ERROR: Master failed, cannot initiate switchover")
 	http.Error(w, "Master failed", http.StatusBadRequest)
+	return
+}
+
+func handlerFailover(w http.ResponseWriter, r *http.Request) {
+	masterFailover(true)
+	return
+}
+
+func handlerInteractiveToggle(w http.ResponseWriter, r *http.Request) {
+	if interactive == true {
+		interactive = false
+	} else {
+		interactive = true
+	}
 	return
 }
