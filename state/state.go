c7745abe7c32d511fcf14d186be6ddbdac546c19
Guillaume Lefranc <guillaume@dragonscale.eu>
2016-08-08 12:21:47 +0200

    Formatting

--- state/state.go
+++ state/state.go
@@ -5,7 +5,6 @@ import "time"
 import "strconv"
 import "sync"
 
-
 type State struct {
 	ErrType string
 	ErrDesc string
@@ -47,7 +46,7 @@ type StateMachine struct {
 	uptime              int64
 	uptimeFailable      int64
 	uptimeSemisync      int64
-  lastState 					int64
+	lastState           int64
 	avgReplicationDelay float32
 	sync.Mutex
 }
@@ -62,11 +61,11 @@ func (SM *StateMachine) Init() {
 	SM.uptime = 0
 	SM.uptimeFailable = 0
 	SM.uptimeSemisync = 0
-  SM.lastState = 0
+	SM.lastState = 0
 }
 
 func (SM *StateMachine) AddState(key string, s State) {
-  SM.Lock()
+	SM.Lock()
 	SM.CurState.Add(key, s)
 	SM.Unlock()
 }
@@ -96,24 +95,24 @@ func (SM *StateMachine) GetUptimeFailable() string {
 }
 
 func (SM *StateMachine) IsFailable() bool {
- return SM.CanMonitor()
+	return SM.CanMonitor()
 }
 
 func (SM *StateMachine) SetMasterUpAndSync(IsSemiSynced bool, IsNotDelay bool) {
 	var timenow int64
 	timenow = time.Now().Unix()
 	if IsSemiSynced == true && SM.IsFailable() == true {
-	  SM.uptimeSemisync = SM.uptimeSemisync + (timenow - SM.lasttime)
+		SM.uptimeSemisync = SM.uptimeSemisync + (timenow - SM.lasttime)
 	}
-	if IsNotDelay==true && SM.IsFailable() == true {
+	if IsNotDelay == true && SM.IsFailable() == true {
 		SM.uptimeFailable = SM.uptimeFailable + (timenow - SM.lasttime)
 	}
-  if SM.IsFailable() == true 	{
+	if SM.IsFailable() == true {
 		SM.uptime = SM.uptime + (timenow - SM.lasttime)
 	}
 	SM.lasttime = timenow
 
-  //fmt.Printf("INFO : is failable %b IsSemiSynced %b  IsNotDelay %b uptime %d uptimeFailable %d uptimeSemisync %d\n",SM.IsFailable(),IsSemiSynced ,IsNotDelay, SM.uptime, SM.uptimeFailable ,SM.uptimeSemisync)
+	//fmt.Printf("INFO : is failable %b IsSemiSynced %b  IsNotDelay %b uptime %d uptimeFailable %d uptimeSemisync %d\n",SM.IsFailable(),IsSemiSynced ,IsNotDelay, SM.uptime, SM.uptimeFailable ,SM.uptimeSemisync)
 }
 
 // Clear copies the current map to argument map and clears it
@@ -122,42 +121,39 @@ func (SM *StateMachine) ClearState() {
 	SM.OldState = SM.CurState
 	SM.CurState = nil
 	SM.CurState = NewMap()
-  SM.Unlock()
+	SM.Unlock()
 }
 
+// CanMonitor checks if the current state contains errors and allows monitoring
 func (SM *StateMachine) CanMonitor() bool {
- SM.Lock()
+	SM.Lock()
 	for _, value := range *SM.CurState {
- 		if value.ErrType == "ERROR" {
-      SM.Unlock()
+		if value.ErrType == "ERROR" {
+			SM.Unlock()
 			return false
 		}
 	}
 	SM.discovered = true
-  SM.Unlock()
+	SM.Unlock()
 	return true
-
 }
 
 func (SM *StateMachine) IsDiscovered() bool {
-
 	return SM.discovered
-
 }
 
 func (SM *StateMachine) GetState() []string {
-
 	var log []string
 	for key2, value2 := range *SM.OldState {
 		if SM.CurState.Search(key2) == false {
-			log = append(log, fmt.Sprintf("%s: %s HAS BEEN FIXED, %s", value2.ErrType, key2, value2.ErrDesc))
+			log = append(log, fmt.Sprintf("%-5s: %s HAS BEEN FIXED, %s", value2.ErrType, key2, value2.ErrDesc))
 
 		}
 	}
 
 	for key, value := range *SM.CurState {
 		if SM.OldState.Search(key) == false {
-			log = append(log, fmt.Sprintf("%s: %s %s", value.ErrType, key, value.ErrDesc))
+			log = append(log, fmt.Sprintf("%-5s: %s %s", value.ErrType, key, value.ErrDesc))
 
 		}
 	}
