38f213b47311528ab0b11f59671f283faa423e61
Guillaume Lefranc <guillaume@dragonscale.eu>
2016-09-21 14:32:35 +0200

    Add systemd service file

--- package.sh
+++ package.sh
@@ -10,9 +10,11 @@ rm *.rpm
 mkdir -p build/usr/bin
 mkdir -p build/usr/share/replication-manager/dashboard
 mkdir -p build/etc/replication-manager
+mkdir -p build/etc/systemd/system
 cp replication-manager build/usr/bin/
 cp etc/config.toml.sample build/etc/replication-manager/config.toml.sample
 cp dashboard/* build/usr/share/replication-manager/dashboard/
+cp service/replication-manager.service build/etc/systemd/system
 fpm --epoch $epoch --iteration $head -v 0.7.0 -C build -s dir -t rpm -n replication-manager .
 fpm --package replication-manager-0.7.0-$head.tar -C build -s dir -t tar -n replication-manager .
 gzip replication-manager-0.7.0-$head.tar
