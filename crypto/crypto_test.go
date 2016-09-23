20d1fc4ecc7c2b2b5cfeeb1fbe42a5f6413b1384
Guillaume Lefranc <guillaume@dragonscale.eu>
2016-09-23 12:57:33 +0200

    Complete test coverage for crypto

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -2,5 +2,19 @@ package crypto
 
 import "testing"
 
-func main(t *testing.T) {
+func TestEncryptDecrypt(t *testing.T) {
+	varpass := "mypass"
+	p := Password{PlainText: varpass}
+	var err error
+	p.Key, err = Keygen()
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.Encrypt()
+	t.Log("Encrypted password is", p.CipherText)
+	p.PlainText = ""
+	p.Decrypt()
+	if p.PlainText != varpass {
+		t.Fatalf("Decrypted password %s differs from initial password", p.PlainText)
+	}
 }
