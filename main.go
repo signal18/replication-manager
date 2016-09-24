signal18/replication-manager

commit a5a19bff16e7f61c679fad963cd46b1fc50c3711

Handle config errors

main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -27,7 +28,10 @@ func init() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath("/etc/replication-manager/")
 	viper.AddConfigPath(".")
-	viper.ReadInConfig()
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln("ERROR: Could not parse config file:", err)
+	}
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.PersistentFlags().StringVar(&user, "user", "", "User for MariaDB login, specified in the [user]:[password] format")
 	rootCmd.PersistentFlags().StringVar(&hosts, "hosts", "", "List of MariaDB hosts IP and port (optional), specified in the host:[port] format and separated by commas")

commit 5da28aa9d2825fd12f4908a1b0831793d966a495

Fix build issue introduced by a5a19bf - Absence of config file caused repmgr to fail

main.go
@@ -29,7 +29,7 @@ func init() {
 	viper.AddConfigPath("/etc/replication-manager/")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
-	if err != nil {
+	if _, ok := err.(viper.ConfigParseError); ok {
 		log.Fatalln("ERROR: Could not parse config file:", err)
 	}
 	rootCmd.AddCommand(versionCmd)