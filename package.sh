505a6509fc2ce7334d34c4c558152dc4be42abc3
Guillaume Lefranc <guillaume@dragonscale.eu>
2016-11-18 21:49:49 +0100

    Update init files

--- package.sh
+++ package.sh
@@ -11,11 +11,14 @@ mkdir -p build/usr/bin
 mkdir -p build/usr/share/replication-manager/dashboard
 mkdir -p build/etc/replication-manager
 mkdir -p build/etc/systemd/system
+mkdir -p build/etc/init.d
 cp replication-manager build/usr/bin/
 cp etc/config.toml.sample build/etc/replication-manager/config.toml.sample
 cp -r dashboard/* build/usr/share/replication-manager/dashboard/
 cp service/replication-manager.service build/etc/systemd/system
+cp service/replication-manager.init.el6 build/etc/init.d/replication-manager
 fpm --epoch $epoch --iteration $head -v 0.7.0 -C build -s dir -t rpm -n replication-manager .
 fpm --package replication-manager-0.7.0-$head.tar -C build -s dir -t tar -n replication-manager .
 gzip replication-manager-0.7.0-$head.tar
+cp service/replication-manager.init.deb7 build/etc/init.d/replication-manager
 fpm --epoch $epoch --iteration $head -v 0.7.0 -C build -s dir -t deb -n replication-manager .
