1b9dbcbd1850a13972f3721c028e6a244cc2b800
svaroqui <stephane@mariadb.com>
2017-02-18 23:42:29 +0100

    Split and fixinf tests

--- cluster/regressiontest.go
+++ cluster/regressiontest.go
@@ -29,7 +29,6 @@ var tests = []string{
 	"testFailoverReplAllDelayAutoRejoinFlashback",
 	"testSwitchoverReplAllDelay",
 	"testSlaReplAllSlavesStopNoSemiSync",
-	"testSlaReplOneSlavesStop",
 	"testSwitchOverReadOnlyNoRplCheck",
 	"testSwitchOverNoReadOnlyNoRplCheck",
 	"testSwitchOver2TimesReplicationOkNoSemiSyncNoRplCheck",
@@ -39,7 +38,6 @@ var tests = []string{
 	"testSwitchOverAllSlavesStopNoSemiSyncNoRplCheck",
 	"testSwitchOverAllSlavesDelayRplCheckNoSemiSync",
 	"testSwitchOverAllSlavesDelayNoRplChecksNoSemiSync",
-	"testSwitchOverAllSlavesDelayRplChecksNoSemiSync",
 	"testFailOverAllSlavesDelayNoRplChecksNoSemiSync",
 	"testFailOverAllSlavesDelayRplChecksNoSemiSync",
 	"testFailOverNoRplChecksNoSemiSync",
@@ -63,109 +61,6 @@ func (cluster *Cluster) testFailoverReplAllDelayInteractive() bool {
 
 func (cluster *Cluster) testSwitchoverReplAllDelay() bool {
 	return false
-}
-
-func (cluster *Cluster) testSlaReplOneSlavesStop() bool {
-	for _, s := range cluster.slaves {
-		dbhelper.StopSlave(s.Conn)
-	}
-	return false
-}
-
-func (cluster *Cluster) testFailOverAllSlavesDelayRplChecksNoSemiSync() bool {
-
-	cluster.Bootstrap()
-	cluster.waitFailoverEnd()
-
-	cluster.LogPrintf("TESTING : Starting Test %s", "testFailOverAllSlavesDelayRplChecksNoSemiSync")
-	for _, s := range cluster.servers {
-		_, err := s.Conn.Exec("set global rpl_semi_sync_master_enabled='OFF'")
-		if err != nil {
-			cluster.LogPrintf("TESTING : %s", err)
-		}
-		_, err = s.Conn.Exec("set global rpl_semi_sync_slave_enabled='OFF'")
-		if err != nil {
-			cluster.LogPrintf("TESTING : %s", err)
-		}
-	}
-	SaveMasterURL := cluster.master.URL
-
-	for _, s := range cluster.slaves {
-		dbhelper.StopSlave(s.Conn)
-	}
-	result, err := dbhelper.WriteConcurrent2(cluster.master.DSN, 10)
-	if err != nil {
-		cluster.LogPrintf("BENCH : %s %s", err.Error(), result)
-	}
-	dbhelper.InjectLongTrx(cluster.master.Conn, 10)
-	time.Sleep(10 * time.Second)
-	for _, s := range cluster.slaves {
-		dbhelper.StartSlave(s.Conn)
-	}
-	cluster.LogPrintf("INFO :  Master is %s", cluster.master.URL)
-
-	cluster.master.State = stateFailed
-	cluster.conf.Interactive = false
-	cluster.master.FailCount = cluster.conf.MaxFail
-	cluster.conf.FailLimit = 5
-	cluster.conf.FailTime = 0
-	cluster.failoverCtr = 0
-	cluster.conf.RplChecks = true
-	cluster.conf.MaxDelay = 4
-	cluster.checkfailed()
-
-	cluster.waitFailoverEnd()
-	cluster.LogPrintf("INFO : New Master  %s ", cluster.master.URL)
-
-	time.Sleep(2 * time.Second)
-	if cluster.master.URL != SaveMasterURL {
-		cluster.LogPrintf("INFO : Old master %s ==  New master %s  ", SaveMasterURL, cluster.master.URL)
-
-		return false
-	}
-	cluster.Bootstrap()
-	cluster.waitFailoverEnd()
-	return true
-}
-
-func (cluster *Cluster) testFailOverNoRplChecksNoSemiSync() bool {
-	cluster.conf.MaxDelay = 0
-	cluster.Bootstrap()
-	cluster.waitFailoverEnd()
-
-	cluster.LogPrintf("TESTING : Starting Test %s", "testFailOverNoRplChecksNoSemiSync")
-	for _, s := range cluster.servers {
-		_, err := s.Conn.Exec("set global rpl_semi_sync_master_enabled='OFF'")
-		if err != nil {
-			cluster.LogPrintf("TESTING : %s", err)
-		}
-		_, err = s.Conn.Exec("set global rpl_semi_sync_slave_enabled='OFF'")
-		if err != nil {
-			cluster.LogPrintf("TESTING : %s", err)
-		}
-	}
-	SaveMasterURL := cluster.master.URL
-
-	cluster.LogPrintf("INFO :  Master is %s", cluster.master.URL)
-	cluster.master.State = stateFailed
-	cluster.conf.Interactive = false
-	cluster.master.FailCount = cluster.conf.MaxFail
-	cluster.conf.FailLimit = 5
-	cluster.conf.FailTime = 0
-	cluster.failoverCtr = 0
-	cluster.conf.RplChecks = false
-	cluster.conf.MaxDelay = 4
-	cluster.checkfailed()
-
-	cluster.waitFailoverEnd()
-	cluster.LogPrintf("INFO : New Master  %s ", cluster.master.URL)
-	if cluster.master.URL == SaveMasterURL {
-		cluster.LogPrintf("INFO : Old master %s ==  Next master %s  ", SaveMasterURL, cluster.master.URL)
-
-		return false
-	}
-
-	return true
 }
 
 func (cluster *Cluster) RunAllTests(test string) bool {
