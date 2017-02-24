77f98c0f18ebafa1950902f3acf4ce6ad69918db
svaroqui <stephane@mariadb.com>
2017-02-24 14:09:41 +0100

    adding test test_failover_assync_norplchecks_masterhearbeat.go

---
+++ cluster/test_failover_assync_norplchecks_masterhearbeat.go
@@ -0,0 +1,44 @@
+package cluster
+
+func (cluster *Cluster) testFailoverNoRplChecksNoSemiSyncMasterHeartbeat(conf string, test string) bool {
+	if cluster.initTestCluster(conf, test) == false {
+		return false
+	}
+	cluster.conf.MaxDelay = 0
+
+	err := cluster.disableSemisync()
+	if err != nil {
+		cluster.LogPrintf("ERROR : %s", err)
+		cluster.closeTestCluster(conf, test)
+		return false
+	}
+	SaveMasterURL := cluster.master.URL
+
+	cluster.LogPrintf("INFO :  Master is %s", cluster.master.URL)
+	cluster.master.State = stateFailed
+	cluster.conf.Interactive = false
+	cluster.master.FailCount = cluster.conf.MaxFail
+	cluster.conf.FailLimit = 5
+	cluster.conf.FailTime = 0
+	cluster.failoverCtr = 0
+	cluster.conf.RplChecks = false
+	cluster.conf.MaxDelay = 20
+	cluster.conf.CheckFalsePositiveHeartbeat = true
+	cluster.checkfailed()
+
+	cluster.waitFailoverEnd()
+	cluster.LogPrintf("INFO : New Master  %s ", cluster.master.URL)
+	if cluster.master.URL != SaveMasterURL {
+		cluster.LogPrintf("INFO : Old master %s ==  Next master %s  ", SaveMasterURL, cluster.master.URL)
+		cluster.closeTestCluster(conf, test)
+		return false
+	}
+	err = cluster.enableSemisync()
+	if err != nil {
+		cluster.LogPrintf("ERROR : %s", err)
+		cluster.closeTestCluster(conf, test)
+		return false
+	}
+	cluster.closeTestCluster(conf, test)
+	return true
+}
