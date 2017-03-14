c575a1d7470df107478666d6e17686f91896ee44
svaroqui <stephane@mariadb.com>
2017-03-14 17:49:47 +0100

    Fixed rejoin slave pointing to dead master

--- cluster/monitor.go
+++ cluster/monitor.go
@@ -294,47 +294,53 @@ func (server *ServerMonitor) check(wg *sync.WaitGroup) {
 			server.State = stateUnconn
 		}
 		return
-	} else if err == nil && ss.Slave_IO_Running == "No" && dbhelper.IsSlaveof(server.Conn, server.Host, server.ClusterGroup.master.IP) == false && server.PrevState == stateFailed {
-		// if a slave not connected to cuurrent master
-		realmaster := server.ClusterGroup.master
-		slave_gtid := server.CurrentGtid.GetSeqServerIdNos(uint64(server.MasterServerID))
-		master_gtid := realmaster.FailoverIOGtid.GetSeqServerIdNos(uint64(server.MasterServerID))
-		if slave_gtid == master_gtid {
-			server.ClusterGroup.LogPrintf("DEBUG: Rejoining slave server %s to master %s", server.URL, realmaster.URL)
-			err = dbhelper.StopSlave(server.Conn)
-			if err == nil {
-				err = dbhelper.ChangeMaster(server.Conn, dbhelper.ChangeMasterOpt{
-					Host:      realmaster.IP,
-					Port:      realmaster.Port,
-					User:      server.ClusterGroup.rplUser,
-					Password:  server.ClusterGroup.rplPass,
-					Retry:     strconv.Itoa(server.ClusterGroup.conf.ForceSlaveHeartbeatRetry),
-					Heartbeat: strconv.Itoa(server.ClusterGroup.conf.ForceSlaveHeartbeatTime),
-					Mode:      "CURRENT_POS",
-				})
-				if err == nil {
-					dbhelper.StartSlave(server.Conn)
-				} else {
-					server.ClusterGroup.LogPrintf("ERROR: Failed to autojoin indirect slave server %s, stopping slave as a precaution.", server.URL)
-					server.ClusterGroup.LogPrint(err)
-				}
-			} else {
-				server.ClusterGroup.LogPrint(err)
-			}
-		} else if server.ClusterGroup.conf.LogLevel > 2 && slave_gtid < master_gtid {
-			server.ClusterGroup.LogPrintf("DEBUG: Slave server %s (%d) is behind master %s (%d)", server.URL, slave_gtid, realmaster.URL, master_gtid)
-		}
-	}
+	} else if err == nil && ss.Slave_IO_Running != "Yes" && dbhelper.IsSlaveof(server.Conn, server.Host, server.ClusterGroup.master.IP) == false && server.PrevState == stateFailed {
+		// if a slave not connected to current master
+		mycurrentmaster, _ := server.ClusterGroup.getMasterFromReplication(server)
+		if mycurrentmaster != nil {
+			if mycurrentmaster.State == stateFailed && mycurrentmaster.IsRelay == false {
+				realmaster := server.ClusterGroup.master
+				slave_gtid := server.CurrentGtid.GetSeqServerIdNos(uint64(server.MasterServerID))
+				master_gtid := realmaster.FailoverIOGtid.GetSeqServerIdNos(uint64(server.MasterServerID))
+				if slave_gtid == master_gtid {
+					server.ClusterGroup.LogPrintf("DEBUG: Rejoining slave server %s to master %s", server.URL, realmaster.URL)
+					err = dbhelper.StopSlave(server.Conn)
+					if err == nil {
+						err = dbhelper.ChangeMaster(server.Conn, dbhelper.ChangeMasterOpt{
+							Host:      realmaster.IP,
+							Port:      realmaster.Port,
+							User:      server.ClusterGroup.rplUser,
+							Password:  server.ClusterGroup.rplPass,
+							Retry:     strconv.Itoa(server.ClusterGroup.conf.ForceSlaveHeartbeatRetry),
+							Heartbeat: strconv.Itoa(server.ClusterGroup.conf.ForceSlaveHeartbeatTime),
+							Mode:      "CURRENT_POS",
+						})
+						if err == nil {
+							dbhelper.StartSlave(server.Conn)
+						} else {
+							server.ClusterGroup.LogPrintf("ERROR: Failed to autojoin indirect slave server %s, stopping slave as a precaution.", server.URL)
+							server.ClusterGroup.LogPrint(err)
+						}
+					} else {
+						server.ClusterGroup.LogPrint(err)
+					}
 
-	// In case of state change, reintroduce the server in the slave list
-	if server.PrevState == stateFailed || server.PrevState == stateUnconn {
-		server.State = stateSlave
-		server.FailCount = 0
-		server.ClusterGroup.slaves = append(server.ClusterGroup.slaves, server)
-		if server.ClusterGroup.conf.ReadOnly {
-			err = dbhelper.SetReadOnly(server.Conn, true)
-			if err != nil {
-				server.ClusterGroup.LogPrintf("ERROR: Could not set rejoining slave %s as read-only, %s", server.URL, err)
+				} else if server.ClusterGroup.conf.LogLevel > 2 && slave_gtid < master_gtid {
+					server.ClusterGroup.LogPrintf("DEBUG: Slave server %s (%d) is behind master %s (%d)", server.URL, slave_gtid, realmaster.URL, master_gtid)
+				}
+			}
+		}
+
+		// In case of state change, reintroduce the server in the slave list
+		if server.PrevState == stateFailed || server.PrevState == stateUnconn {
+			server.State = stateSlave
+			server.FailCount = 0
+			server.ClusterGroup.slaves = append(server.ClusterGroup.slaves, server)
+			if server.ClusterGroup.conf.ReadOnly {
+				err = dbhelper.SetReadOnly(server.Conn, true)
+				if err != nil {
+					server.ClusterGroup.LogPrintf("ERROR: Could not set rejoining slave %s as read-only, %s", server.URL, err)
+				}
 			}
 		}
 	}
