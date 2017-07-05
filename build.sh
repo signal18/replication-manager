86af9e232cbedbcaf051178fa6b22c94bbd678e3
Guillaume Lefranc <guillaume@signal18.io>
2017-07-05 17:29:24 +0200

    Set verbose build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ VERSION=$(git describe --abbrev=0 --tags)
 FULLVERSION=$(git describe --tags)
 BUILD=$(date +%FT%T%z)
 
-go build -ldflags "-w -s -X main.Version=${VERSION} -X main.FullVersion=${FULLVERSION} -X main.Build=${BUILD}" ${LDFLAGS} -o ${BINARY}
+go build -v -ldflags "-w -s -X main.Version=${VERSION} -X main.FullVersion=${FULLVERSION} -X main.Build=${BUILD}" ${LDFLAGS} -o ${BINARY}
