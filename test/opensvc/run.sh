c4fd436d601b4ed42942f3ef972c2e0053d8126e
svaroqui <stephane@mariadb.com>
2017-08-03 13:25:46 +0200

    Put test result in build version directory

--- test/opensvc/run.sh
+++ test/opensvc/run.sh
@@ -2,25 +2,27 @@
 version=$(git describe --tags)
 for i in $(find ./$1 -name "*.conf") ; do
   testdir=$(dirname "${i}")
-  echo  "{\"Build\":\"$version\", \"setups\":[" > $testdir/result-$version.json
-  echo "{\"name\":\"$i\", \"results\":[" >> $testdir/result-$version.json
+  destdir=$testdir/$version
+  mkdir $destdir
+
+  echo "{\"results\":[" >> $destdir/result.json
   echo $testdir
   tests=`cat $testdir/tests.todo`
   for test in $tests ; do
    > $testdir/$test.log
-   ../../replication-manager-pro --test --logfile=$testdir/$test.log --config=./$i monitor  &
+   ../../replication-manager-pro --test --logfile=$destdir/$test.log --config=./$i monitor  &
    pid="$!"
    sleep 3
    while [[ $(../../replication-manager-pro api --url=https://127.0.0.1:3000/api/status) != "{\"alive\": \"running\"}" ]] ; do
     echo "waiting start service"
     sleep 1
    done
-    ../../replication-manager-pro test --run-tests="$test" >> $testdir/result-$version.json
+    ../../replication-manager-pro test --run-tests="$test" >> $testdir/result.json
    kill $pid
-   echo ","  >> $testdir/result-$version.json
+   echo ","  >> $destdir/result.json
 
   done
-  echo "]},"  >> $testdir/result-$version.json
-  echo "]}" >> $testdir/result-$version.json
+  echo "]},"  >> $destdir/result.json
+
 
 done
