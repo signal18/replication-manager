4b7fad94d74c1bcec6c0b072fe068891ff9125b2
Guillaume Lefranc <guillaume@signal18.io>
2017-09-09 16:14:19 +0200

    Backport fixes for packaging tool

--- package_linux_amd64.sh
+++ package_linux_amd64.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
+
+nobuild=0
+if [ "$1" != "" ]; then
+  if [ "$1" = "--no-build" ]; then
+    nobuild=1
+  fi
+fi
+
 echo "# Getting branch info"
 git status -bs
-echo "# Press Return or Space to start build, all other keys to quit"
-read -s -n 1 key
-if [[ $key != "" ]]; then exit; fi
-version=$(git describe --tag)
+
+version=$(git describe --tag --abbrev=4)
 head=$(git rev-parse --short HEAD)
 epoch=$(date +%s)
-echo "# Building"
-./build_linux_amd64.sh
+description="Replication Manager for MariaDB and MySQL"
+maintainer="info@signal18.io"
+license="GPLv3"
+
+if [ $nobuild -eq 0 ]; then
+  echo "# Building"
+  ./build_linux_amd64.sh
+fi
+
 echo "# Cleaning up previous builds"
 rm -rf build
+rm -rf buildtar
 rm *.tar.gz
 rm *.deb
 rm *.rpm
 mkdir -p build/usr/bin
+
+cflags=(-m "$maintainer" --license "$license" -v $version)
+
 mkdir -p build/usr/share/replication-manager/dashboard
 mkdir -p build/etc/replication-manager
 mkdir -p build/etc/systemd/system
 mkdir -p build/etc/init.d
 mkdir -p build/var/lib/replication-manager
+mkdir -p buildtar/bin
+mkdir -p buildtar/etc
+mkdir -p buildtar/share
+mkdir -p buildtar/data
+
 echo "# Copying files to build dir"
-cp replication-manager build/usr/bin/
-cp etc/* build/etc/replication-manager/
 cp -r dashboard/* build/usr/share/replication-manager/dashboard/
 cp -r share/* build/usr/share/replication-manager/
-rm -rf build/usr/share/replication-manager/opensvc
-cp service/replication-manager.service build/etc/systemd/system
+
+echo "# Building replication-manager packages"
+cp etc/* build/etc/replication-manager/
+cp replication-manager build/usr/bin/
+cp service/replication-manager.service build/etc/systemd/system/replication-manager.service
 cp service/replication-manager.init.el6 build/etc/init.d/replication-manager
-echo "# Building packages"
-fpm --rpm-os linux --epoch $epoch --iteration $head -v $version -C build -s dir -t rpm -n replication-manager .
-fpm --package replication-manager-$version-$head.tar -C build -s dir -t tar -n replication-manager .
-gzip replication-manager-$version-$head.tar
+cp service/replication-manager-arbitrator.init.el6 build/etc/init.d/replication-manager-arbitrator
+fpm ${cflags[@]} --rpm-os linux -C build -s dir -t rpm -n replication-manager --epoch $epoch --description "$description" .
 cp service/replication-manager.init.deb7 build/etc/init.d/replication-manager
-fpm --epoch $epoch --iteration $head -v $version -C build -s dir -t deb -n replication-manager .
+cp service/replication-manager-arb.init.deb7 build/etc/init.d/replication-manager-arbitrator
+fpm ${cflags[@]} -C build -s dir -t deb -n replication-manager --description "$description" .
+rm -f build/usr/bin/replication-manager
+
+echo "# Building replication-manager tarball"
+cp etc/* buildtar/etc/
+cp replication-manager buildtar/bin/
+cp service/replication-manager.service buildtar/share/replication-manager.service
+cp service/replication-manager.init.el6 buildtar/share/replication-manager.init
+fpm --package replication-manager-$version.tar --prefix replication-manager -C buildtar -s dir -t tar -n replication-manager .
+gzip replication-manager-$version.tar
+rm -rf buildtar/bin/replication-manager
+
+echo "# Build complete"
