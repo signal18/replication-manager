75e09c9402a5845ecbbe8d04a5d6e4381592aaf4
Guillaume Lefranc <guillaume@signal18.io>
2017-10-04 21:28:24 +0200

    Remove redundant dependencies

--- vendor/github.com/lomik/go-carbon/vendor/github.com/dgryski/httputil/doc.go
+++
@@ -1,2 +0,0 @@
-// Package httputil is a collection of utilities for net/http handlers
-package httputil
--- vendor/github.com/lomik/go-carbon/vendor/github.com/hydrogen18/stalecucumber/python/versions.py
+++
@@ -1,4 +0,0 @@
-import pickletools
-v0 = [opcode for opcode in pickletools.opcodes if opcode.proto == 0]
-v1 = [opcode for opcode in pickletools.opcodes if opcode.proto == 1] 
-v2 = [opcode for opcode in pickletools.opcodes if opcode.proto == 2]
