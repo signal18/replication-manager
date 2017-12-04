912f7f199cc34db896cc495b1f2e268ef53bddc2
Guillaume Lefranc <guillaume@signal18.io>
2017-12-04 12:02:11 +0100

    Clarify error message when repl privs not found

--- dbhelper/dbhelper.go
+++ dbhelper/dbhelper.go
@@ -354,8 +354,10 @@ func GetPrivileges(db *sqlx.DB, user string, host string, ip string) (Privileges
 	iprange3 := splitip[0] + "." + splitip[1] + "." + splitip[2] + ".%"
 	stmt := "SELECT MAX(Select_priv) as Select_priv, MAX(Process_priv) as Process_priv, MAX(Super_priv) as Super_priv, MAX(Repl_slave_priv) as Repl_slave_priv, MAX(Repl_client_priv) as Repl_client_priv, MAX(Reload_priv) as Reload_priv FROM mysql.user WHERE user = ? AND host IN(?,?,?,?,?,?,?,?,?)"
 	row := db.QueryRowx(stmt, user, host, ip, "%", ip+"/255.0.0.0", ip+"/255.255.0.0", ip+"/255.255.255.0", iprange1, iprange2, iprange3)
-	//	fmt.Println("'" + user + "',''" + host + "','" + ip + "', ''" + ip + "/255.0.0.0'" + ", ''" + ip + "/255.255.0.0'" + "','" + ip + "/255.255.255.0" + "','" + iprange1 + "','" + iprange2 + "','" + iprange3)
 	err := row.StructScan(&priv)
+    if strings.Contains(err.Error(), "unsupported Scan") {
+        return priv, errors.New("No replication user defined. Please check the replication user is created with the required privileges")
+    }
 	return priv, err
 }
 
