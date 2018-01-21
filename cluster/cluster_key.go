950a60a7fe6466332c5956b3b23d3dae5aeece31
svaroqui <stephane@signal18.io>
2018-01-21 17:33:35 +0100

    Don't create SSL key fro cluster yet

--- cluster/cluster_key.go
+++ cluster/cluster_key.go
@@ -33,83 +33,84 @@ func (cluster *Cluster) CreateKey() error {
 
 	//	r := router{PKI: &easypki.EasyPKI{Store: &store.Local{}}}
 	//	var signer *certificate.Bundle
+	if false {
+		reader := rand.Reader
+		bitSize := 2048
 
-	reader := rand.Reader
-	bitSize := 2048
-
-	key, err := rsa.GenerateKey(reader, bitSize)
-	if err != nil {
-		return err
-	}
-	priv := key
-	// start generate key
-	var notBefore time.Time
-	notBefore = time.Now()
-	notAfter := notBefore.Add(365 * 24 * time.Hour * 2)
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		log.Fatalf("failed to generate serial number: %s", err)
-	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"Signa18"},
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	for _, h := range cluster.servers {
-		if ip := net.ParseIP(h.Host); ip != nil {
-			template.IPAddresses = append(template.IPAddresses, ip)
+		key, err := rsa.GenerateKey(reader, bitSize)
+		if err != nil {
+			return err
 		}
-	}
-	template.IsCA = true
-	template.KeyUsage |= x509.KeyUsageCertSign
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
-	if err != nil {
-		log.Fatalf("Failed to create certificate: %s", err)
-	}
+		priv := key
+		// start generate key
+		var notBefore time.Time
+		notBefore = time.Now()
+		notAfter := notBefore.Add(365 * 24 * time.Hour * 2)
+		serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+		serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+		if err != nil {
+			log.Fatalf("failed to generate serial number: %s", err)
+		}
 
-	certOut, err := os.Create(cluster.conf.WorkingDir + "/" + cluster.cfgGroup + "/cert.pem")
-	if err != nil {
-		log.Fatalf("failed to open cert.pem for writing: %s", err)
-	}
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-	keyOut, err := os.OpenFile(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Print("failed to open key.pem for writing:", err)
+		template := x509.Certificate{
+			SerialNumber: serialNumber,
+			Subject: pkix.Name{
+				Organization: []string{"Signa18"},
+			},
+			NotBefore: notBefore,
+			NotAfter:  notAfter,
+
+			KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+			ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+			BasicConstraintsValid: true,
+		}
+
+		for _, h := range cluster.servers {
+			if ip := net.ParseIP(h.Host); ip != nil {
+				template.IPAddresses = append(template.IPAddresses, ip)
+			}
+		}
+		template.IsCA = true
+		template.KeyUsage |= x509.KeyUsageCertSign
+		derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
+		if err != nil {
+			log.Fatalf("Failed to create certificate: %s", err)
+		}
+
+		certOut, err := os.Create(cluster.conf.WorkingDir + "/" + cluster.cfgGroup + "/cert.pem")
+		if err != nil {
+			log.Fatalf("failed to open cert.pem for writing: %s", err)
+		}
+		pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+		certOut.Close()
+		keyOut, err := os.OpenFile(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			log.Print("failed to open key.pem for writing:", err)
+			return err
+		}
+		pem.Encode(keyOut, pemBlockForKey(priv))
+		keyOut.Close()
+
+		// End certificate
+
+		publicKey := key.PublicKey
+
+		err = cluster.saveGobKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/private.key", key)
+		if err != nil {
+			return err
+		}
+		err = cluster.savePEMKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/private.pem", key)
+		if err != nil {
+			return err
+		}
+		err = cluster.saveGobKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/public.key", publicKey)
+		if err != nil {
+			return err
+		}
+		err = cluster.savePublicPEMKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/public.pem", publicKey)
 		return err
 	}
-	pem.Encode(keyOut, pemBlockForKey(priv))
-	keyOut.Close()
-
-	// End certificate
-
-	publicKey := key.PublicKey
-
-	err = cluster.saveGobKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/private.key", key)
-	if err != nil {
-		return err
-	}
-	err = cluster.savePEMKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/private.pem", key)
-	if err != nil {
-		return err
-	}
-	err = cluster.saveGobKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/public.key", publicKey)
-	if err != nil {
-		return err
-	}
-	err = cluster.savePublicPEMKey(cluster.conf.WorkingDir+"/"+cluster.cfgGroup+"/public.pem", publicKey)
-
-	return err
+	return nil
 }
 
 func (cluster *Cluster) saveCaCert(fileName string, key interface{}) error {
