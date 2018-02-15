8e3853e0cf3fc352c3e444001a7bae78d16c27ff
Gabriel Ajabahian <gajabahian@ripe.net>
2018-02-15 21:53:31 +0100

    Adding a frontend service.

---
+++ dashboard/app/app.service.js
@@ -0,0 +1,24 @@
+app.service('AppService', ['$http', '$localStorage', function ($http, $localStorage) {
+
+    var getApiDomain = function(){
+        return 'https://localhost:10005/api';
+    };
+
+    var setAuthenticated = function (user, token) {
+        // store username and token in local storage to keep user logged in between page refreshes
+        $localStorage.currentUser = { username: user, token: token };
+
+        // add jwt token to auth header for all requests made by the $http service
+        $http.defaults.headers.common.Authorization = 'Bearer ' + token;
+    };
+
+    var hasAuthHeaders = function () {
+        return ($http.defaults.headers.common.Authorization);
+    };
+
+    return {
+        getApiDomain: getApiDomain,
+        setAuthenticated: setAuthenticated,
+        hasAuthHeaders: hasAuthHeaders
+    };
+}]);
