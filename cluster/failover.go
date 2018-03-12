55e13bc2ef3f8e9778ffa70bfc06282401d882df
svaroqui <stephane@signal18.io>
2018-03-12 10:18:06 +0100

    Switchover failure need current_pos as slave_pos is not updated on master

--- cluster/failover.go
+++ cluster/failover.go
@@ -391,6 +391,7 @@ func (cluster *Cluster) MasterFailover(fail bool) bool {
 			}
 		} else if cluster.conf.MxsBinlogOn == false {
 			cluster.LogPrintf(LvlInfo, "Doing MariaDB GTID switch of the old master")
+			// current pos is needed on old master as  writes diverges from slave pos
 			changeMasterErr = dbhelper.ChangeMaster(oldMaster.Conn, dbhelper.ChangeMasterOpt{
 				Host:      cluster.master.Host,
 				Port:      cluster.master.Port,
@@ -398,7 +399,7 @@ func (cluster *Cluster) MasterFailover(fail bool) bool {
 				Password:  cluster.rplPass,
 				Retry:     strconv.Itoa(cluster.conf.ForceSlaveHeartbeatRetry),
 				Heartbeat: strconv.Itoa(cluster.conf.ForceSlaveHeartbeatTime),
-				Mode:      "SLAVE_POS",
+				Mode:      "CURRENT_POS",
 				SSL:       cluster.conf.ReplicationSSL,
 			})
 			if changeMasterErr != nil {
