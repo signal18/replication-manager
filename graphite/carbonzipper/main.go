23449d7862c01925b62083aae0191a08b564d928
Guillaume Lefranc <guillaume@signal18.io>
2018-07-26 12:01:06 +0200

    Remove all copyrights on graphite files

--- graphite/carbonzipper/main.go
+++ graphite/carbonzipper/main.go
@@ -1,8 +1,3 @@
-// replication-manager - Replication Manager Monitoring and CLI for MariaDB and MySQL
-// Copyright 2017 Signal 18 SARL
-// Authors: Guillaume Lefranc <guillaume@signal18.io>
-//          Stephane Varoqui  <svaroqui@gmail.com>
-// This source code is licensed under the GNU General Public License, version 3.
 
 package main
 
--- graphite/persister/whisper_schema.go
+++ graphite/persister/whisper_schema.go
@@ -1,8 +1,3 @@
-// replication-manager - Replication Manager Monitoring and CLI for MariaDB and MySQL
-// Copyright 2017 Signal 18 SARL
-// Authors: Guillaume Lefranc <guillaume@signal18.io>
-//          Stephane Varoqui  <svaroqui@gmail.com>
-// This source code is licensed under the GNU General Public License, version 3.
 
 // this is a parser for graphite's storage-schemas.conf
 // it supports old and new retention format
