16a349ccd6bccca70f3ed622981f1f416a63bd20
svaroqui <stephane@signal18.io>
2019-05-31 18:11:47 +0200

    Fixing compile

---
+++ utils/misc/files.go
@@ -0,0 +1,15 @@
+package misc
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func ChownR(path string, uid, gid int) error {
+	return filepath.Walk(path, func(name string, info os.FileInfo, err error) error {
+		if err == nil {
+			err = os.Chown(name, uid, gid)
+		}
+		return err
+	})
+}
