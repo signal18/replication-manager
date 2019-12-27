247c6224cdea1e73e4520296b253969733d90ed2
svaroqui <stephane@signal18.io>
2019-12-27 17:23:30 +0100

    Don't set unconnected when galera

--- cluster/srv.go
+++ cluster/srv.go
@@ -421,9 +421,9 @@ func (server *ServerMonitor) Ping(wg *sync.WaitGroup) {
 					}
 				}
 			}
-			//if server.IsRunning() {
-			server.State = stateUnconn
-			//}
+			if server.ClusterGroup.GetTopology() != topoMultiMasterWsrep {
+				server.State = stateUnconn
+			}
 			server.FailCount = 0
 			server.ClusterGroup.backendStateChangeProxies()
 			server.SendAlert()
