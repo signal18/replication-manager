0f346b202723afa1b16c7fc3abbc75fedc8df574
vegazmaoning <50688156+vegazmaoning@users.noreply.github.com>
2020-03-02 23:52:31 +0800

    fix goroutine leak when connection mysql err

--- cluster/srv.go
+++ cluster/srv.go
@@ -229,7 +229,11 @@ func (server *ServerMonitor) Ping(wg *sync.WaitGroup) {
 			err = fmt.Errorf("HTTP Response Code Error: %d", resp.StatusCode)
 		}
 	}
-
+	
+	if conn != nil {
+		defer conn.Close()
+	}
+	
 	// Handle failure cases here
 	if err != nil {
 		server.setDSN()
@@ -288,9 +292,7 @@ func (server *ServerMonitor) Ping(wg *sync.WaitGroup) {
 
 		return
 	}
-	if conn != nil {
-		defer conn.Close()
-	}
+	
 	// from here we have connection
 	server.Refresh()
 
