451616e266eddafbdccc345140b8c68aa1ab8b43
svaroqui <stephane@signal18.io>
2020-04-09 14:59:08 +0200

    Cleanup compliance for proxysql

--- cluster/prx_get.go
+++ cluster/prx_get.go
@@ -178,7 +178,7 @@ optional=true
 	return vm
 }
 
-func (proxy *Proxy) GetBndAddress() string {
+func (proxy *Proxy) GetBindAddress() string {
 	if proxy.ClusterGroup.Conf.ProvOrchestrator == config.ConstOrchestratorSlapOS {
 		return proxy.Host
 	}
@@ -200,7 +200,7 @@ func (proxy *Proxy) GetEnv() map[string]string {
 		"%%ENV:SVC_CONF_ENV_SERVER_ID%%":               string(proxy.Id[2:10]),
 		"%%ENV:SVC_CONF_ENV_MYSQL_ROOT_PASSWORD%%":     proxy.ClusterGroup.dbPass,
 		"%%ENV:SVC_CONF_ENV_MYSQL_ROOT_USER%%":         proxy.ClusterGroup.dbUser,
-		"%%ENV:SERVER_IP%%":                            proxy.GetBndAddress(),
+		"%%ENV:SERVER_IP%%":                            proxy.GetBindAddress(),
 		"%%ENV:SERVER_PORT%%":                          proxy.Port,
 		"%%ENV:SVC_NAMESPACE%%":                        proxy.ClusterGroup.Name,
 		"%%ENV:SVC_NAME%%":                             proxy.Name,
