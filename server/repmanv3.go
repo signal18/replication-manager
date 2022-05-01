dc161b157a9cc38719d091727f53bc8efba0de43
Yorick Terweijden <terwey@gmail.com>
2022-05-01 19:52:23 +0200

    Fix Issue #424, CipherSuites now supported in 1.16 too

--- server/repmanv3.go
+++ server/repmanv3.go
@@ -289,17 +289,19 @@ func (s *ReplicationManager) getCredentials() (opts []grpc.ServerOption, dopts [
 			ServerName:               s.v3Config.Listen.Address, // this is critical
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
+			PreferServerCipherSuites: false,
 			CipherSuites: []uint16{
 				tls.TLS_AES_128_GCM_SHA256,
 				tls.TLS_AES_256_GCM_SHA384,
 				tls.TLS_CHACHA20_POLY1305_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+
+				// only supported in TLS1.2
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
 			},
 		}
 		// in case the certificate is self-signed we must add the certificate to the TLS' known pool of CA's
