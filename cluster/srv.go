cb9c3c6ad89ec0a7b520b740b2ec8d69905cf4b2
apple <stephane@signal18.io>
2023-02-21 11:09:10 +0100

    Don't collect query digest if no PFS

--- cluster/srv.go
+++ cluster/srv.go
@@ -726,7 +726,7 @@ func (server *ServerMonitor) Refresh() error {
 			server.EngineInnoDB, logs, err = dbhelper.GetEngineInnoDBVariables(server.Conn)
 			server.ClusterGroup.LogSQL(logs, err, server.URL, "Monitor", LvlDbg, "Could not get engine innodb status %s %s", server.URL, err)
 		}
-		if server.ClusterGroup.Conf.MonitorPFS {
+		if server.ClusterGroup.Conf.MonitorPFS && server.HavePFSSlowQueryLog && server.HavePFS {
 			// GET PFS query digest
 			server.PFSQueries, logs, err = dbhelper.GetQueries(server.Conn)
 			server.ClusterGroup.LogSQL(logs, err, server.URL, "Monitor", LvlDbg, "Could not get queries %s %s", server.URL, err)
