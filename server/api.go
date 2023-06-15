e9eba91d614375bd2bf295495b2a55b0ee03c0d0
eloubersac <emma@signal18.io>
2023-06-15 10:11:17 +0200

    create a gitlab project for user if not exist

--- server/api.go
+++ server/api.go
@@ -17,11 +17,9 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
 
@@ -37,6 +35,7 @@ import (
 	"github.com/signal18/replication-manager/cluster"
 	"github.com/signal18/replication-manager/regtest"
 	"github.com/signal18/replication-manager/share"
+	"github.com/signal18/replication-manager/utils/githelper"
 )
 
 //RSA KEYS AND INITIALISATION
@@ -364,129 +363,6 @@ func (repman *ReplicationManager) loginHandler(w http.ResponseWriter, r *http.Re
 
 }
 
-type TokenInfo struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	UserID int    `json:"user_id"`
-	Token  string `json:"token"`
-}
-type NameSpace struct {
-	ID int `json:"id"`
-}
-
-type ProjectInfo struct {
-	ID            int       `json:"id"`
-	Name          string    `json:"name"`
-	PathNameSpace string    `json:"path_with_namespace"`
-	Path          string    `json:"path"`
-	Namespace     NameSpace `json:"namespace"`
-}
-
-func handlerGetGitLabTokenOAuth(w http.ResponseWriter, r *http.Request, acces_token string) string {
-
-	// curl --request GET --header "Authorization: Bearer XXX" "https://gitlab.signal18.io/api/v4/personal_access_tokens?revoked=false"
-
-	req, err := http.NewRequest("GET", "https://gitlab.signal18.io/api/v4/personal_access_tokens?revoked=false", nil)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-	}
-	req.Header.Set("Authorization", "Bearer "+acces_token)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return ""
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println("Gitlab API Response: ", string(body))
-
-	var tokenInfos []TokenInfo
-
-	err = json.Unmarshal(body, &tokenInfos)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return ""
-	}
-
-	id := strconv.Itoa(tokenInfos[0].ID)
-
-	// curl --request POST --header "Authorization: Bearer 592d333b9a44357ff238ecfc13a1920290bb28c57026f48494ae6e09853d556a" "https://gitlab.signal18.io/api/v4/personal_access_tokens/11/rotate"
-
-	req, err = http.NewRequest("POST", "https://gitlab.signal18.io/api/v4/personal_access_tokens/"+id+"/rotate", nil)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return ""
-	}
-	req.Header.Set("Authorization", "Bearer "+acces_token)
-
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return ""
-	}
-	defer resp.Body.Close()
-	body, _ = ioutil.ReadAll(resp.Body)
-	//log.Println("Gitlab API Response: ", string(body))
-
-	err = json.Unmarshal(body, &tokenInfos[0])
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return ""
-	}
-	return tokenInfos[0].Token
-
-}
-
-func handlerGitLabCreateProject(token string, name string, path string, namespace string) {
-	req, err := http.NewRequest("GET", "https://gitlab.signal18.io/api/v4/projects?search="+name, nil)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-	}
-	req.Header.Set("Private-token", token)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println("Gitlab API Response: ", string(body))
-
-	var ProjectInfos []ProjectInfo
-
-	err = json.Unmarshal(body, &ProjectInfos)
-	if err != nil {
-		log.Println("Gitlab API Error: ", err)
-		return
-	}
-
-	if len(ProjectInfos) != 0 {
-		project_path := ProjectInfos[0].PathNameSpace
-		if project_path == path {
-			return
-		} else {
-			jsondata := "{'name': " + name + ", 'description': '', 'path': " + name + ",'namespace': " + namespace + ", 'initialize_with_readme': 'true'}"
-			b := bytes.NewBuffer([]byte(jsondata))
-			req, err := http.NewRequest("POST", "https://gitlab.example.com/api/v4/projects/", b)
-			if err != nil {
-				log.Println("Gitlab API Error: ", err)
-				return
-			}
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Private-token", token)
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				log.Println("Gitlab API Error: ", err)
-				return
-			}
-			defer resp.Body.Close()
-			body, _ := ioutil.ReadAll(resp.Body)
-			log.Println("Gitlab API Response: ", string(body))
-		}
-	}
-}
-
 func (repman *ReplicationManager) handlerMuxAuthCallback(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	OAuthContext := context.Background()
@@ -526,13 +402,13 @@ func (repman *ReplicationManager) handlerMuxAuthCallback(w http.ResponseWriter, 
 			cluster.APIUsers[userInfo.Email] = apiuser
 
 			if cluster.Conf.Cloud18 {
-				new_token := handlerGetGitLabTokenOAuth(w, r, oauth2Token.AccessToken)
+				new_token, user_id := githelper.GetGitLabTokenOAuth(oauth2Token.AccessToken)
 
 				if new_token != "" {
 					//to create project for user if not exist
 					path := cluster.Conf.Cloud18Domain + "/" + cluster.Conf.Cloud18SubDomain + "-" + cluster.Conf.Cloud18SubDomainZone
 					name := cluster.Conf.Cloud18SubDomain + "-" + cluster.Conf.Cloud18SubDomainZone
-					handlerGitLabCreateProject(new_token, name, path, cluster.Conf.Cloud18Domain)
+					githelper.GitLabCreateProject(new_token, name, path, cluster.Conf.Cloud18Domain, user_id)
 					//to store new gitlab token
 					cluster.Conf.GitUrl = repman.Conf.OAuthProvider + "/" + cluster.Conf.Cloud18Domain + "/" + cluster.Conf.Cloud18SubDomain + "-" + cluster.Conf.Cloud18SubDomainZone + ".git"
 					cluster.Conf.GitUsername = tmp[len(tmp)-1]
