bb8c08df453ca2cbf158beffd7bf798a7f75b644
caffeinated92 <ahmad@signal18.io>
2024-05-29 09:34:15 +0700

    Revert "add ssh port and binlog directory in case of using SSH"
    This reverts commit c122d82265e99cce49cce5be6376dea62d6abe52.

--- share/scripts/binlog_copy.sh
+++ share/scripts/binlog_copy.sh
@@ -6,8 +6,8 @@ set -e
 
 # Binlog Copy Script only get the previous binlog to backup
 echo "Binlog copy script args"
-echo "Script:$0, Cluster:$1, Server:$2, MySQL Port:$3, SSH Port: $4, Source Binlog Path: $5, Destination Path (Repman): $6, Binlog Filename: $7"
+echo "Script:$0, Cluster:$1, Server:$2, MySQL Port:$3, Backup Directory: $4, Binlog:$5"
 
-echo "Write dummy logs as binlog" > "$6/$7"
+echo "Write dummy logs as binlog" > "$4/$5"
 
 echo "Write dummy logs as binlog completed"
