20326c73f335aa7e9c0f1199b69315076e6f801d
S18 Dev User <marie@mixr-fr-5.signal18.io>
2024-07-12 21:33:52 +0200

    made navbar fixed, logs completed

---
+++ share/dashboard_react/src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import { Box, Flex, Image, Button, Spacer, Text, HStack, useColorMode, IconButton, background } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../redux/authSlice'
+import ThemeIcon from './ThemeIcon'
+import RefreshCounter from './RefreshCounter'
+import { isAuthorized } from '../utility/common'
+import { Link } from 'react-router-dom'
+import { useTheme } from '@emotion/react'
+
+function Navbar({ username, theme }) {
+  const dispatch = useDispatch()
+  const {
+    common: { isMobile, isTablet, isDesktop }
+  } = useSelector((state) => state)
+
+  const currentTheme = useTheme()
+
+  const styles = {
+    navbarContainer: {
+      boxShadow: theme === 'dark' ? 'none' : '0px -1px 8px #BFC1CB',
+      position: 'fixed',
+      zIndex: 2,
+      width: '100%',
+      background: theme === 'light' ? currentTheme.colors.primary.light : currentTheme.colors.primary.dark
+    },
+    logo: {
+      bg: '#eff2fe',
+      borderRadius: '4px'
+    }
+  }
+
+  const handleLogout = () => {
+    dispatch(logout())
+  }
+  return (
+    <>
+      <Flex as='nav' p='10px' sx={styles.navbarContainer} gap='2' align='center'>
+        <Link to='/'>
+          <Image
+            loading='lazy'
+            height='50px'
+            width={isMobile ? '180px' : 'fit-content'}
+            sx={styles.logo}
+            objectFit='contain'
+            src='/images/logo.png'
+            alt='Replication
+           Manager'
+          />
+        </Link>
+        <Spacer />
+
+        {isAuthorized() && isDesktop && <RefreshCounter />}
+
+        <Spacer />
+        <HStack spacing='4'>
+          {isAuthorized() && (
+            <>
+              {username && isDesktop && <Text>{`Welcome, ${username}`}</Text>}{' '}
+              <Button type='button' size={{ base: 'sm' }} onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          )}
+
+          <ThemeIcon theme={theme} />
+        </HStack>
+      </Flex>
+      {isAuthorized() && !isDesktop && (
+        <Box mx='auto' p='16px' marginTop='80px'>
+          <RefreshCounter />
+        </Box>
+      )}
+    </>
+  )
+}
+
+export default Navbar
