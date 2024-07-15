0197ce8a7c7e423fbae3960156245f2157a091fd
S18 Dev User <marie@mixr-fr-5.signal18.io>
2024-07-15 16:37:28 +0200

    database server tables in progress

--- share/dashboard_react/src/Pages/Dashboard/index.jsx
+++ share/dashboard_react/src/Pages/Dashboard/index.jsx
@@ -60,6 +60,7 @@ function Dashboard({ selectedCluster }) {
         panelSX={{ overflowX: 'auto', p: 0 }}
         body={<Proxies selectedCluster={selectedCluster} />}
       />
+      <AccordionComponent heading={'Databasr servers'} body={<DBServers />} />
       <AccordionComponent heading={'Cluster Logs'} body={<Logs logs={selectedCluster?.log?.buffer} />} />
       <AccordionComponent heading={'Job Logs'} body={<Logs logs={selectedCluster?.logTask?.buffer} />} />
     </Flex>
