9911f71760800874bae4c2820938cc4f989b568b
S18 Dev User <marie@mixr-fr-5.signal18.io>
2024-07-19 01:02:00 +0200

    gridview and compare servers done

--- share/dashboard_react/src/Pages/Dashboard/components/DBServers/index.jsx
+++ share/dashboard_react/src/Pages/Dashboard/components/DBServers/index.jsx
@@ -20,6 +20,7 @@ function DBServers({ selectedCluster, user }) {
     common: { isDesktop },
     cluster: { clusterServers, clusterMaster }
   } = useSelector((state) => state)
+  const { colorMode } = useColorMode()
   const [data, setData] = useState([])
   const [viewType, setViewType] = useState('table')
   const [hasMariadbGtid, setHasMariadbGtid] = useState(false)
