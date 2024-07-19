fdb2378c19d712769312abf081e235605c186a77
S18 Dev User <marie@mixr-fr-5.signal18.io>
2024-07-20 01:52:22 +0200

    fixed accordion behaviour on grid

--- share/dashboard_react/src/components/AccordionComponent.jsx
+++ share/dashboard_react/src/components/AccordionComponent.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react'
 
-function AccordionComponent({ heading, body, panelSX, headerSX }) {
+function AccordionComponent({ heading, body, panelSX, headerSX, isOpen = null, onToggle }) {
   const styles = {
     panel: {
       padding: 0
     }
   }
   return (
-    <Accordion allowToggle={true} defaultIndex={0}>
+    <Accordion allowToggle={true} defaultIndex={0} {...(isOpen !== null ? { index: isOpen ? [0] : [] } : {})}>
       <AccordionItem>
         <h2>
-          <AccordionButton sx={headerSX}>
+          <AccordionButton sx={headerSX} {...(onToggle ? { onClick: onToggle } : {})}>
             <Box as='span' flex='1' textAlign='left'>
               {heading}
             </Box>
