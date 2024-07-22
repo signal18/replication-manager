fc6d83edfd4c54d3649e1e7dc288461f3806480d
S18 Dev User <marie@mixr-fr-5.signal18.io>
2024-07-22 16:07:34 +0200

    fixed gauge

--- share/dashboard_react/src/components/Gauge.jsx
+++ share/dashboard_react/src/components/Gauge.jsx
@@ -1,21 +1,50 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import { useTheme } from '@emotion/react'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import GaugeComponent from 'react-gauge-component'
 
-function Gauge({ value, text, width, containerSx }) {
+function Gauge({ value, text, width, height, containerSx }) {
+  const svgRef = useRef(null)
+
+  const updateGaugePosition = () => {
+    const svgElement = svgRef.current.querySelector('svg')
+    if (svgElement) {
+      const gElements = svgElement.querySelectorAll('g')
+      gElements.forEach((g) => {
+        const transform = g.getAttribute('transform')
+        const translateMatch = /translate\(([^,]+),\s*([^)]+)\)/.exec(transform)
+        if (translateMatch) {
+          const [_, x, y] = translateMatch
+          g.setAttribute('transform', `translate(${x}, 30)`)
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    updateGaugePosition()
+  }, [value, updateGaugePosition])
+
+  useEffect(() => {
+    window.addEventListener('resize', updateGaugePosition)
+    return () => {
+      window.removeEventListener('resize', updateGaugePosition)
+    }
+  }, [])
+
   const styles = {
     container: {
       width: `${width}px`,
+      height: `${height}px`,
       position: 'relative',
       display: 'inline-block'
     },
     gauge: {
-      width: '100%'
+      height: '100%'
     },
     textOverlay: {
       position: 'absolute',
-      bottom: '-10px',
+      bottom: '-20px',
       left: '50%',
       transform: 'translate(-50%, -50%)',
       textAlign: 'center'
@@ -39,7 +68,7 @@ function Gauge({ value, text, width, containerSx }) {
 
   return (
     <Flex direction='column' justify='center'>
-      <Box sx={styles.container}>
+      <Box sx={styles.container} ref={svgRef}>
         <GaugeComponent
           style={styles.guage}
           value={value}
@@ -57,9 +86,6 @@ function Gauge({ value, text, width, containerSx }) {
           <Text sx={styles.labelText}>{text}</Text>
         </Box>
       </Box>
-      {/* <Text textAlign='center' fontWeight='500'>
-        {text}
-      </Text> */}
     </Flex>
   )
 }
