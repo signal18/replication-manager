d80a3022e60eecca480a677fe3194525da1f7104
caffeinated92 <ahmad@signal18.io>
2024-09-14 20:19:11 +0700

    make into one function

--- server/api_database.go
+++ server/api_database.go
@@ -765,7 +765,6 @@ func (repman *ReplicationManager) handlerMuxServerSetMaintenance(w http.Response
 		node := mycluster.GetServerFromName(vars["serverName"])
 		if node != nil {
 			node.SetMaintenance()
-			mycluster.SetProxyServerMaintenance(node.ServerID)
 		} else {
 			http.Error(w, "Server Not Found", 500)
 			return
@@ -788,7 +787,6 @@ func (repman *ReplicationManager) handlerMuxServerDelMaintenance(w http.Response
 		node := mycluster.GetServerFromName(vars["serverName"])
 		if node != nil {
 			node.DelMaintenance()
-			mycluster.SetProxyServerMaintenance(node.ServerID)
 		} else {
 			http.Error(w, "Server Not Found", 500)
 			return
