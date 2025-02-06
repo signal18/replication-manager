6c965addaa71aa6f2974dbfee1fd7ea692b78d6a
caffeinated92 <ahmad@signal18.io>
2025-02-06 18:27:26 +0700

    Update ACL check for terminal API to match exact URL

--- cluster/cluster_acl.go
+++ cluster/cluster_acl.go
@@ -606,7 +606,7 @@ func (cluster *Cluster) IsURLPassACL(strUser string, URL string, errorPrint bool
 		return true
 	}
 
-	if strings.Contains(URL, "/api/terminal") {
+	if URL == "/api/terminal" {
 		return cluster.APIUsers[strUser].Grants[config.GrantGlobalTerminal]
 	}
 	if strings.Contains(URL, "/api/clusters/settings/actions/switch") {
