9ed1a3c76167b2786e800105b3e3a7d34918fa28
caffeinated92 <ahmad@signal18.io>
2025-03-13 14:19:11 +0700

    feat: add methods to manage cluster apps including addition and removal

--- cluster/cluster_app.go
+++ cluster/cluster_app.go
@@ -1,6 +1,8 @@
 package cluster
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/signal18/replication-manager/cluster/app"
@@ -21,6 +23,43 @@ func (cluster *Cluster) DiscoverClusterApps() {
 		cluster.Apps = append(cluster.Apps, newapp)
 		cluster.AppsIdList = append(cluster.AppsIdList, newapp.Id)
 	}
+}
 
-	return
+// AddClusterApp adds a new app to the cluster
+func (cluster *Cluster) AddClusterApp(host string) {
+	if !cluster.Conf.ConfRewrite {
+		cluster.Logrus.Info("Unable to add apps without dynamic config")
+		return
+	}
+
+	newapp := app.NewAppInstance(cluster, len(cluster.Apps), host)
+	cluster.Logrus.Infof("Adding app %s to cluster %s", newapp.Name, cluster.Name)
+	cluster.Apps = append(cluster.Apps, newapp)
+	cluster.AppsIdList = append(cluster.AppsIdList, newapp.Id)
+}
+
+func (cluster *Cluster) RemoveAppMonitor(host string, port string) error {
+	newApps := make([]*app.App, 0)
+	newAppHosts := make([]string, 0)
+	index := -1
+	for i, pr := range cluster.Apps {
+		if pr.GetHost() == host && pr.GetPort() == port {
+			index = i
+		} else {
+			newApps = append(newApps, pr)
+			newAppHosts = append(newAppHosts, pr.HostCnf)
+		}
+	}
+	if index >= 0 {
+		cluster.StateMachine.SetFailoverState()
+		cluster.Lock()
+		cluster.Apps = newApps
+		cluster.Conf.AppHosts = strings.Join(newAppHosts, ",")
+		cluster.Unlock()
+		cluster.StateMachine.RemoveFailoverState()
+	} else {
+		return errors.New(fmt.Sprintf("App host with address %s:%s not found in cluster!", host, port))
+	}
+
+	return nil
 }
