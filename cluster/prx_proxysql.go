263bb616dcbbc9245cf96b524608367f5efa3daf
caffeinated92 <ahmad@signal18.io>
2025-04-15 11:18:37 +0700

    Handle standalone server behavior for non staging ProxySQL based within staging topology

--- cluster/prx_proxysql.go
+++ cluster/prx_proxysql.go
@@ -464,14 +464,21 @@ func (proxy *ProxySQLProxy) Refresh() error {
 				updated = true
 
 			} else if s.State == stateUnconn && bkeread.PrxStatus == "ONLINE" && IsBackendReader {
-				// if server is Standalone, and reader shunned it in ProxySQL
-				cluster.LogModulePrintf(cluster.Conf.Verbose, config.ConstLogModProxySQL, config.LvlInfo, "Monitor ProxySQL setting reader offline standalone server %s", s.URL)
-				err = psql.SetOfflineSoft(misc.Unbracket(s.Host), s.Port)
-				if err != nil {
-					cluster.SetState("ERR00070", state.State{ErrType: "WARNING", ErrDesc: fmt.Sprintf(clusterError["ERR00070"], err, s.URL), ErrFrom: "PRX", ServerUrl: proxy.Name})
+				if cluster.Conf.TopologyStaging { // Need to be dropped since standalone should not be writing in staging topology
+					cluster.LogModulePrintf(cluster.Conf.Verbose, config.ConstLogModProxySQL, config.LvlDbg, "Monitor Non Staging ProxySQL: drop standalone in reader group from %s", s.URL)
+					err = psql.DropReader(misc.Unbracket(s.Host), s.Port)
+					if err != nil {
+						cluster.SetState("ERR00070", state.State{ErrType: "WARNING", ErrDesc: fmt.Sprintf(clusterError["ERR00070"], err, s.URL), ErrFrom: "PRX", ServerUrl: proxy.Name})
+					}
+				} else {
+					// if server is Standalone, and reader shunned it in ProxySQL
+					cluster.LogModulePrintf(cluster.Conf.Verbose, config.ConstLogModProxySQL, config.LvlInfo, "Monitor ProxySQL setting reader offline standalone server %s", s.URL)
+					err = psql.SetOfflineSoft(misc.Unbracket(s.Host), s.Port)
+					if err != nil {
+						cluster.SetState("ERR00070", state.State{ErrType: "WARNING", ErrDesc: fmt.Sprintf(clusterError["ERR00070"], err, s.URL), ErrFrom: "PRX", ServerUrl: proxy.Name})
+					}
+					updated = true
 				}
-				updated = true
-
 			} else if s.IsLeader() && (s.PrevState == stateUnconn || s.PrevState == stateFailed || (len(proxy.BackendsWrite) == 0 || !isBackendWriter)) {
 				// if the master comes back from a previously failed or standalone state, reintroduce it in
 				// the appropriate HostGroup
