c28005f943934f1b2ffbc778ed04e967a68dc0ae
caffeinated92 <ahmad@signal18.io>
2025-04-29 19:08:23 +0700

    Add logging for default database service printing in PrintDefaultDatabaseServices method

--- cluster/cluster_job.go
+++ cluster/cluster_job.go
@@ -248,6 +248,7 @@ func (cluster *Cluster) PrintDefaultDatabaseServices(regenerate bool) error {
 	defer cluster.LogPanicToFile("printdefault")
 	for _, srv := range cluster.Servers {
 		if srv != nil {
+			cluster.LogModulePrintf(cluster.Conf.Verbose, config.ConstLogModTask, config.LvlInfo, "Print default database service for %s", srv.URL)
 			err := cluster.PrintDefaultDatabaseService(srv, regenerate)
 			if err != nil {
 				if regenerate {
