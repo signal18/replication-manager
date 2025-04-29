4c15c6fd6690749062a8e0232ddcaa5c5dd9c28a
caffeinated92 <ahmad@signal18.io>
2025-04-29 22:14:19 +0700

    Refactor copyLogsPrefix to accept multiple prefixes for log filtering

--- cluster/srv_job.go
+++ cluster/srv_job.go
@@ -2323,7 +2323,7 @@ func (server *ServerMonitor) copyLogs(r io.Reader, module int, level string) {
 	}
 }
 
-func (server *ServerMonitor) copyLogsPrefix(r io.Reader, module int, level string, prefix string) {
+func (server *ServerMonitor) copyLogsPrefix(r io.Reader, module int, level string, prefix ...string) {
 	cluster := server.ClusterGroup
 	//	buf := make([]byte, 1024)
 	s := bufio.NewScanner(r)
@@ -2332,8 +2332,10 @@ func (server *ServerMonitor) copyLogsPrefix(r io.Reader, module int, level strin
 			break
 		} else {
 			//Remove empty lines
-			if strings.HasPrefix(s.Text(), prefix) {
-				cluster.LogModulePrintf(cluster.Conf.Verbose, module, level, "[%s] %s", server.Name, strings.TrimPrefix(s.Text(), prefix))
+			for _, p := range prefix {
+				if strings.HasPrefix(s.Text(), p) {
+					cluster.LogModulePrintf(cluster.Conf.Verbose, module, level, "[%s] %s", server.Name, strings.TrimPrefix(s.Text(), p))
+				}
 			}
 		}
 	}
