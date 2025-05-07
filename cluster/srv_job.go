e6ef3c420825ab3a2b624e9567e480e5853babc7
caffeinated92 <ahmad@signal18.io>
2025-05-07 20:22:50 +0700

    Update secret encryption to include JSON structure and enhance secret decoding with improved error handling

--- cluster/srv_job.go
+++ cluster/srv_job.go
@@ -3396,5 +3396,21 @@ func (server *ServerMonitor) DecodeSecret(encrypted, key, iv string) (string, er
 		return "", err
 	}
 
-	return strings.TrimSpace(decryptedOutput.String()), nil
+	output := decryptedOutput.Bytes()
+	pos := bytes.LastIndex(output, []byte("}"))
+	if pos > 1 {
+		output = output[:pos+1]
+	}
+
+	var secretKey struct {
+		Secret string `json:"secret"`
+	}
+
+	err = json.Unmarshal(output, &secretKey)
+	if err != nil {
+		cluster.LogModulePrintf(cluster.Conf.Verbose, config.ConstLogModTask, config.LvlErr, "Error loading JSON Entry: %s. Err: %s", output, err.Error())
+		return "", err
+	}
+
+	return strings.TrimSpace(secretKey.Secret), nil
 }
