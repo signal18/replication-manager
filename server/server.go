a9baf422819274b580b1153b301df2bcaf07018b
caffeinated92 <ahmad@signal18.io>
2025-06-13 10:16:50 +0700

    feat: update default disk size for microservice VM provisioning

--- server/server.go
+++ server/server.go
@@ -1067,7 +1067,7 @@ func (repman *ReplicationManager) AddFlags(flags *pflag.FlagSet, conf *config.Co
 	flags.StringVar(&conf.AppHostsIPV6, "app-hosts-ipv6", "", "App IPv6 bind address ")
 	flags.IntVar(&conf.LogAppLevel, "app-log-level", 3, "Log level for application")
 	flags.StringVar(&conf.ProvAppAgents, "prov-app-agents", "", "App agents")
-	flags.StringVar(&conf.ProvAppDisk, "prov-app-disk-size", "3", "Disk in g for micro service VM")
+	flags.StringVar(&conf.ProvAppDisk, "prov-app-disk-size", "4G", "Disk in g for micro service VM")
 	flags.StringVar(&conf.ProvAppCores, "prov-app-cpu-cores", "1", "Cpu cores ")
 	flags.StringVar(&conf.ProvAppMem, "prov-app-memory", "1G", "Memory usage in M bytes")
 	flags.StringVar(&conf.ProvAppVolumeData, "prov-app-volume-data", "tank", "Volume name of the data files")
