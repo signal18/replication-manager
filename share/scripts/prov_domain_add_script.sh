80f15ee75b502938e3b6731781861b8035a9eca6
caffeinated92 <ahmad@signal18.io>
2025-06-18 15:20:15 +0700

    Fix argument order and update API token variable in prov_domain_add_script.sh

--- share/scripts/prov_domain_add_script.sh
+++ share/scripts/prov_domain_add_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This script is given as sample and will be overwrite on upgrade
 
-ZONE_ID=$2
+ZONE_ID=$1
+API_TOKEN=$2
 ZONE_DNS=$3
 ZONE_GATEWAY=$4
 
@@ -9,4 +10,4 @@ ZONE_GATEWAY=$4
 curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records \
     -d '{ "comment": "", "name": "'$ZONE_DNS'", "content": "'$ZONE_GATEWAY'", "proxied": false, "ttl": 3600, "type": "CNAME" }' \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $2"
+    -H "Authorization: Bearer $API_TOKEN"
