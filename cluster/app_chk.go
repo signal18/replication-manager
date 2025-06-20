45e55a312140a7af4ad6ce2171d6c367a8b5771e
caffeinated92 <ahmad@signal18.io>
2025-06-20 17:02:23 +0700

    Fix primary status initialization in GetMonitoringStatus method

--- cluster/app_chk.go
+++ cluster/app_chk.go
@@ -17,7 +17,7 @@ import (
 
 func (app *App) GetMonitoringStatus() string {
 	routes := app.GetAppConfig().Deployment.Routes
-	var primaryStatus string = app.State
+	var primaryStatus string = stateAppRunning
 	if len(routes) == 0 {
 		return stateFailed
 	}
