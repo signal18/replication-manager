c0df20f8c0a2f74acdb6a2de0df0d2d39436946d
caffeinated92 <ahmad@signal18.io>
2025-11-12 20:19:46 +0700

    fix(cluster): ensure backup server is set and handle missing server case

--- cluster/cluster_bck.go
+++ cluster/cluster_bck.go
@@ -263,7 +263,10 @@ func (cluster *Cluster) CheckBackupFreeSpace(backtype string, backup bool) error
 	var isWarning bool
 	bcksrv := cluster.GetBackupServer()
 	if bcksrv == nil {
-		bcksrv = cluster.master
+		bcksrv = cluster.GetMaster()
+		if bcksrv == nil {
+			return fmt.Errorf("No backup server or master server found for cluster %s", cluster.Name)
+		}
 	}
 
 	parentDir := cluster.Conf.WorkingDir + "/" + config.ConstStreamingSubDir + "/" + cluster.Name
