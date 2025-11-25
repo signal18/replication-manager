e0bca538969d07a227ca2d47f36d92344a7d688c
caffeinated92 <ahmad@signal18.io>
2025-11-25 16:42:07 +0700

    fix(config): ensure terminal session is disabled for non-opensvc orchestrators

--- server/server.go
+++ server/server.go
@@ -1360,7 +1360,13 @@ func (repman *ReplicationManager) InitConfig(conf config.Config, init_git bool) 
 			if v != nil {
 				ImmuableMap[f] = secRead.Get(f)
 			}
+		}
+	}
 
+	_, terminalImmu := ImmuableMap["terminal-session-enabled"]
+	if conf.ProvOrchestrator != "opensvc" {
+		if !terminalImmu {
+			conf.TerminalSessionEnabled = false
 		}
 	}
 
@@ -1417,6 +1423,18 @@ func (repman *ReplicationManager) InitConfig(conf config.Config, init_git bool) 
 	for _, clusterName := range repman.ImmutableClusterList {
 		clRead := fistRead.Sub(clusterName)
 		repman.DeprecatedKeys[clusterName] = repman.GetUsedAliasKeys(clRead, true) //get deprecated keys used in the cluster config dir (/etc/replication-manager/cluster.d)
+		clOrch := conf.ProvOrchestrator
+		if clRead != nil {
+			if v := clRead.Get("prov-orchestrator"); v != nil {
+				clOrch = clRead.GetString("prov-orchestrator")
+			}
+
+			if clOrch != "opensvc" && conf.TerminalSessionEnabled {
+				if v := clRead.Get("terminal-session-enabled"); v == nil {
+					conf.TerminalSessionEnabled = false
+				}
+			}
+		}
 	}
 
 	tmp_read := fistRead.Sub("default")
