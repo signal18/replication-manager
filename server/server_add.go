d0856584163f8f44c4e671662adb1f60ec862829
caffeinated92 <ahmad@signal18.io>
2025-11-25 16:53:46 +0700

    fix(user-form): add 'terminal' grant to admin user form

--- server/server_add.go
+++ server/server_add.go
@@ -48,7 +48,7 @@ func (repman *ReplicationManager) CreateAdminUserForm(username string) cluster.U
 	return cluster.UserForm{
 		Username: username,
 		Roles:    "sysops dbops",
-		Grants:   "cluster db proxy prov global grant show sale extrole app",
+		Grants:   "cluster db proxy prov global grant show sale extrole app terminal",
 	}
 }
 
