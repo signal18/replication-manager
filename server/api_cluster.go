d646ee815f4cbf7d367e23a809e8eb09782d6d69
caffeinated92 <ahmad@signal18.io>
2025-12-02 21:19:06 +0700

    feat(cluster): add replication master retry count setting to configuration UI

--- server/api_cluster.go
+++ server/api_cluster.go
@@ -3295,6 +3295,9 @@ func (repman *ReplicationManager) setClusterSetting(mycluster *cluster.Cluster, 
 		mycluster.Conf.ReplicationMultisourceHeadClusters = value
 	case "replication-source-name":
 		mycluster.Conf.MasterConn = value
+	case "replication-master-retry-count":
+		val, _ := strconv.Atoi(value)
+		mycluster.Conf.MasterRetryCount = val
 	case "db-servers-tls-ssl-mode":
 		mycluster.Conf.HostsTlsSslMode = value
 
