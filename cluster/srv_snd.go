43e070b7c7ecb7200c1c19926d5c132f664bd59e
caffeinated92 <ahmad@signal18.io>
2025-12-02 18:13:32 +0700

    refactor(cluster): simplify FetchDatabaseStats by removing unnecessary variable assignment

--- cluster/srv_snd.go
+++ cluster/srv_snd.go
@@ -93,10 +93,8 @@ func (server *ServerMonitor) GetDatabaseMetrics() []graphite.Metric {
 	return metrics
 }
 
-func (server *ServerMonitor) FetchDatabaseStats() error {
-	metrics := server.GetDatabaseMetrics()
-	server.GetCluster().AddMetrics(metrics)
-	return nil
+func (server *ServerMonitor) FetchDatabaseStats() {
+	server.GetCluster().AddMetrics(server.GetDatabaseMetrics())
 }
 
 func (server *ServerMonitor) SendAlert() error {
