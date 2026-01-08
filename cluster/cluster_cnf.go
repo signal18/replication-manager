9650a37c626c6188114580e71825b3555f41218d
caffeinated92 <ahmad@signal18.io>
2026-01-08 10:05:50 +0000

    feat: remove unused GetPreservedVarsInfo function to streamline preserved variable management

--- cluster/cluster_cnf.go
+++ cluster/cluster_cnf.go
@@ -22,31 +22,6 @@ func (cluster *Cluster) GetPreservedVarsPath() string {
 	return filepath.Join(cluster.Conf.WorkingDir, cluster.Name, "preserved_variables.cnf")
 }
 
-// GetPreservedVarsInfo returns information about currently loaded preserved variables
-func (cluster *Cluster) GetPreservedVarsInfo() map[string]interface{} {
-	cluster.preservedVarsMutex.RLock()
-	defer cluster.preservedVarsMutex.RUnlock()
-
-	info := make(map[string]any)
-	info["loaded"] = cluster.preservedVarsLoaded
-	info["count"] = len(cluster.preservedVars)
-	info["path"] = cluster.GetPreservedVarsPath()
-
-	// Check if file exists
-	preservedPath := cluster.GetPreservedVarsPath()
-	if _, err := os.Stat(preservedPath); err == nil {
-		info["exists"] = true
-		if finfo, err := os.Stat(preservedPath); err == nil {
-			info["modified"] = finfo.ModTime()
-			info["size"] = finfo.Size()
-		}
-	} else {
-		info["exists"] = false
-	}
-
-	return info
-}
-
 // ReloadPreservedVars forces a reload of preserved variables from cluster working directory
 // Useful after manually editing the preserved_variables.cnf file
 func (cluster *Cluster) ReloadPreservedVars() error {
