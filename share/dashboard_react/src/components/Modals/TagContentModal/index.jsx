91bc59db10bf96a2a8e1f0edfb803d77b6bdbe96
svarqoui <stephane@signal18.io>
2026-05-04 14:43:55 +0200

    fix: TagContentModal theme support and scrolling
    - Light theme: light gray background with dark text for the code block
    - Dark theme: dark background with green text (unchanged)
    - Modal uses modalLightContent/modalDarkContent from shared styles
    - Doc links use theme-appropriate colors (blue.600/teal.600 on light)
    - Increased modal size to 3xl for wider content
    - maxH 50vh on code block + 40vh on doc table with overflowY auto

--- share/dashboard_react/src/components/Modals/TagContentModal/index.jsx
+++ share/dashboard_react/src/components/Modals/TagContentModal/index.jsx
@@ -5,15 +5,19 @@ import {
   HStack, Badge
 } from '@chakra-ui/react'
 import { FiExternalLink } from 'react-icons/fi'
+import { useTheme } from '../../../ThemeProvider'
+import parentStyles from '../styles.module.scss'
 
 function TagContentModal({ isOpen, closeModal, tagName, data }) {
   const content = data?.content || ''
   const docHelp = data?.doc_help
+  const { theme } = useTheme()
+  const isLight = theme === 'light'
 
   return (
-    <Modal isOpen={isOpen} onClose={closeModal} size='2xl' scrollBehavior='inside'>
+    <Modal isOpen={isOpen} onClose={closeModal} size='3xl' scrollBehavior='inside'>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent className={isLight ? parentStyles.modalLightContent : parentStyles.modalDarkContent}>
         <ModalHeader>Tag: {tagName}</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
@@ -21,14 +25,16 @@ function TagContentModal({ isOpen, closeModal, tagName, data }) {
             <Box
               as='pre'
               p={4}
-              bg='gray.800'
-              color='green.200'
+              bg={isLight ? 'gray.50' : 'gray.800'}
+              color={isLight ? 'gray.800' : 'green.200'}
+              border='1px solid'
+              borderColor={isLight ? 'gray.200' : 'whiteAlpha.200'}
               borderRadius='md'
               fontSize='sm'
               overflowX='auto'
               whiteSpace='pre-wrap'
               wordBreak='break-all'
-              maxH='40vh'
+              maxH='50vh'
               overflowY='auto'
             >
               <Code bg='transparent' color='inherit' whiteSpace='pre-wrap'>
@@ -41,46 +47,48 @@ function TagContentModal({ isOpen, closeModal, tagName, data }) {
 
           {docHelp?.variables?.length > 0 && (
             <>
-              <Divider my={4} borderColor='whiteAlpha.300' />
-              <Text fontSize='sm' fontWeight='bold' mb={2} color='blue.200'>
+              <Divider my={4} borderColor={isLight ? 'gray.200' : 'whiteAlpha.300'} />
+              <Text fontSize='sm' fontWeight='bold' mb={2} color={isLight ? 'blue.600' : 'blue.200'}>
                 Documentation Links
               </Text>
-              <Table size='sm' variant='simple'>
-                <Thead>
-                  <Tr>
-                    <Th>Variable</Th>
-                    <Th>Documentation</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {docHelp.variables.map((v) => (
-                    <Tr key={v.name}>
-                      <Td>
-                        <Text fontSize='xs' fontFamily='mono'>{v.name}</Text>
-                      </Td>
-                      <Td>
-                        <HStack spacing={2} flexWrap='wrap'>
-                          {v.mariadb_url && (
-                            <Link href={v.mariadb_url} isExternal color='blue.300' fontSize='xs'>
-                              MariaDB <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
-                            </Link>
-                          )}
-                          {v.mysql_url && (
-                            <Link href={v.mysql_url} isExternal color='teal.300' fontSize='xs'>
-                              MySQL <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
-                            </Link>
-                          )}
-                          {v.blogs?.map((blog, i) => (
-                            <Link key={i} href={blog.url} isExternal color='purple.300' fontSize='xs'>
-                              {blog.title} <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
-                            </Link>
-                          ))}
-                        </HStack>
-                      </Td>
+              <Box maxH='40vh' overflowY='auto'>
+                <Table size='sm' variant='simple'>
+                  <Thead>
+                    <Tr>
+                      <Th>Variable</Th>
+                      <Th>Documentation</Th>
                     </Tr>
-                  ))}
-                </Tbody>
-              </Table>
+                  </Thead>
+                  <Tbody>
+                    {docHelp.variables.map((v) => (
+                      <Tr key={v.name}>
+                        <Td>
+                          <Text fontSize='xs' fontFamily='mono'>{v.name}</Text>
+                        </Td>
+                        <Td>
+                          <HStack spacing={2} flexWrap='wrap'>
+                            {v.mariadb_url && (
+                              <Link href={v.mariadb_url} isExternal color={isLight ? 'blue.600' : 'blue.300'} fontSize='xs'>
+                                MariaDB <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
+                              </Link>
+                            )}
+                            {v.mysql_url && (
+                              <Link href={v.mysql_url} isExternal color={isLight ? 'teal.600' : 'teal.300'} fontSize='xs'>
+                                MySQL <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
+                              </Link>
+                            )}
+                            {v.blogs?.map((blog, i) => (
+                              <Link key={i} href={blog.url} isExternal color={isLight ? 'purple.600' : 'purple.300'} fontSize='xs'>
+                                {blog.title} <FiExternalLink style={{ display: 'inline', marginLeft: 2 }} />
+                              </Link>
+                            ))}
+                          </HStack>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </Box>
               {docHelp.unknown_variables?.length > 0 && (
                 <Box mt={2}>
                   <Text fontSize='xs' color='gray.500' mb={1}>
