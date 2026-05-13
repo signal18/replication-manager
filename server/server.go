08cf96296f45b0933d084f3613fe308728c9057f
svarqoui <stephane@signal18.io>
2026-05-13 14:49:49 +0200

    fix: default cloud18-domain-add-script to /usr/share/replication-manager/scripts/prov_domain_add_script.sh

--- server/server.go
+++ server/server.go
@@ -1110,7 +1110,7 @@ func (repman *ReplicationManager) AddFlags(flags *pflag.FlagSet, conf *config.Co
 	flags.StringVar(&conf.Cloud18CrmApiUrl, "cloud18-crm-api-url", "https://api.crm.ovh-fr-2.signal18.cloud18.io", "Cloud18 CRM API base URL used for cluster registration")
 	flags.IntVar(&conf.Cloud18ApplicationCredits, "cloud18-application-credits", 2, "Cloud18 application credits(1 core 4G Ram 8G Disk)")
 	flags.IntVar(&conf.Cloud18ApplicationCreditsPrice, "cloud18-application-credits-price", 20, "Cloud18 application credits price in Eur")
-	flags.StringVar(&conf.Cloud18DomainAddScript, "cloud18-domain-add-script", "", "Script to add DNS CNAME entry to cloud18-gateway-domain-name")
+	flags.StringVar(&conf.Cloud18DomainAddScript, "cloud18-domain-add-script", "/usr/share/replication-manager/scripts/prov_domain_add_script.sh", "Script to add DNS CNAME entry to cloud18-gateway-domain-name")
 	flags.StringVar(&conf.Cloud18DomainDropScript, "cloud18-domain-drop-script", "", "Script to drop DNS CNAME entry to cloud18-gateway-domain-name")
 	flags.StringVar(&conf.Cloud18DomainUser, "cloud18-domain-user", "", "First parameter to pass prov-domain-?-script")
 	flags.StringVar(&conf.Cloud18DomainSecret, "cloud18-domain-secret", "", "Second parameter to pass prov-domain-?-script")
