cb3fa81fb384e41ffc7da945eb0841802054dc92
svarqoui <stephane@signal18.io>
2026-05-30 10:07:39 +0200

    feat(gui): add Enable Configurator toggle at top of Configurator tab

--- share/dashboard_react/src/Pages/Configs/index.jsx
+++ share/dashboard_react/src/Pages/Configs/index.jsx
@@ -3,13 +3,15 @@ import AccordionComponent from '../../components/AccordionComponent'
 import styles from './styles.module.scss'
 import DBConfigs from './components/DBConfigs'
 import ProxyConfig from './components/ProxyConfig'
-import { VStack } from '@chakra-ui/react'
+import { HStack, Text, VStack } from '@chakra-ui/react'
 import OrchestratorImages from './components/OrchestratorImages'
 import OrchestratorDisks from './components/OrchestratorDisks'
 import OrchestratorDbVM from './components/OrchestratorDbVM'
 import Certificates from './components/Certificates'
 import { useDispatch, useSelector } from 'react-redux'
 import { getClusterCertificates } from '../../redux/clusterSlice'
+import { switchSetting } from '../../redux/settingsSlice'
+import RMSwitch from '../../components/RMSwitch'
 
 function Configs({ selectedCluster, user }) {
   const dispatch = useDispatch()
@@ -26,6 +28,17 @@ function Configs({ selectedCluster, user }) {
 
   return (
     <VStack className={styles.configContainer}>
+      <HStack px={4} py={2} spacing={3}>
+        <Text fontWeight={600} fontSize='sm'>Enable Configurator</Text>
+        <RMSwitch
+          isChecked={selectedCluster?.config?.provDbConfig}
+          isDisabled={user?.grants['cluster-settings'] == false}
+          confirmTitle={'Confirm switch settings for prov-db-config? When disabled, no config is deployed to databases but tracking remains visible.'}
+          onChange={() =>
+            dispatch(switchSetting({ clusterName: selectedCluster?.name, setting: 'prov-db-config' }))
+          }
+        />
+      </HStack>
       <AccordionComponent
         className={styles.accordion}
         headerClassName={styles.accordionHeader}
