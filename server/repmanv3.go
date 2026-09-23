43252828c847e926f63c6e98d1210545d4a7b632
caffeinated92 <ahmad@signal18.io>
2026-09-23 08:53:35 +0700

    Guard manual failover while master is healthy

--- server/repmanv3.go
+++ server/repmanv3.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -593,7 +594,7 @@ func (s *ReplicationManager) PerformClusterAction(ctx context.Context, in *v3.Cl
 	case v3.ClusterAction_CHECKSUM_ALL_TABLES:
 		go mycluster.CheckAllTableChecksum()
 	case v3.ClusterAction_FAILOVER:
-		mycluster.MasterFailover(true)
+		err = mycluster.Failover()
 	case v3.ClusterAction_MASTER_PHYSICAL_BACKUP:
 		m := mycluster.GetMaster()
 		if m == nil {
@@ -616,11 +617,8 @@ func (s *ReplicationManager) PerformClusterAction(ctx context.Context, in *v3.Cl
 		mycluster.SetTraffic(false)
 	case v3.ClusterAction_SWITCHOVER:
 		mycluster.LogModulePrintf(mycluster.Conf.Verbose, config.ConstLogModGeneral, "INFO", "API force for prefered master: %s", in.Server.GetURI())
-		if mycluster.IsInHostList(in.Server.GetURI()) {
-			mycluster.SetPrefMaster(in.Server.GetURI())
-			mycluster.MasterFailover(false)
-			return emptyResponse()
-		} else {
+		err = mycluster.Switchover(in.Server.GetURI())
+		if errors.Is(err, cluster.ErrPreferredMasterNotFound) {
 			return nil, v3.NewErrorResource(codes.NotFound, v3.ErrServerNotFound, "Server", in.Server.GetURI()).Err()
 		}
 	case v3.ClusterAction_SYSBENCH:
@@ -633,6 +631,9 @@ func (s *ReplicationManager) PerformClusterAction(ctx context.Context, in *v3.Cl
 
 	if err != nil {
 		mycluster.LogModulePrintf(mycluster.Conf.Verbose, config.ConstLogModGeneral, "ERROR", "API Error: %s", err)
+		if errors.Is(err, cluster.ErrFailoverMasterHealthy) || errors.Is(err, cluster.ErrSwitchoverMasterFailed) {
+			return nil, v3.NewError(codes.FailedPrecondition, err).Err()
+		}
 		return nil, v3.NewError(codes.Unknown, err).Err()
 	}
 
